1cc757b6236902679075d92876cebf49c63748b8
Jihong Park <ggikko@hotmail.com>
2016-04-20 11:10:19 +0900

    inject viewpager & tablayout

--- TabViewpager/app/src/main/java/ggikko/me/tabviewpager/MainActivity.java
+++ TabViewpager/app/src/main/java/ggikko/me/tabviewpager/MainActivity.java
@@ -1,13 +1,26 @@
 package ggikko.me.tabviewpager;
 
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TableLayout;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.tabLayout) TableLayout tabLayout;
+    @Bind(R.id.tab_viewpager) ViewPager tab_viewpager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ButterKnife.bind(this);
+
+
+
     }
 }

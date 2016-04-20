e105f765ff9d2e2292cc62c5ad1138da387573f7
Jihong Park <ggikko@hotmail.com>
2016-04-20 11:18:43 +0900

    add two fragment and tablayout(google design support)

--- TabViewpager/app/src/main/java/ggikko/me/tabviewpager/MainActivity.java
+++ TabViewpager/app/src/main/java/ggikko/me/tabviewpager/MainActivity.java
@@ -1,17 +1,25 @@
 package ggikko.me.tabviewpager;
 
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.widget.TableLayout;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.Bind;
 import butterknife.ButterKnife;
+import ggikko.me.tabviewpager.fragment.FirstFragment;
+import ggikko.me.tabviewpager.fragment.SecondFragment;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Bind(R.id.tabLayout) TableLayout tabLayout;
-    @Bind(R.id.tab_viewpager) ViewPager tab_viewpager;
+    @Bind(R.id.tabLayout) TabLayout tabLayout;
+    @Bind(R.id.tab_viewpager) ViewPager viewPager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,7 +28,72 @@ public class MainActivity extends AppCompatActivity {
 
         ButterKnife.bind(this);
 
+        viewPagerSetting();
 
+        tabLayout.setTabGravity(TabLayout.GRAVITY_FILL);
+        tabLayout.setupWithViewPager(viewPager);
 
+        tabLayoutSetting();
     }
+
+    /** viewpager setting */
+    private void viewPagerSetting() {
+        ViewPagerAdapter adapter = new ViewPagerAdapter(getSupportFragmentManager());
+        adapter.addFrag(new FirstFragment(), "first");
+        adapter.addFrag(new SecondFragment(), "second");
+        viewPager.setAdapter(adapter);
+    }
+
+    /** tablayout setting */
+    private void tabLayoutSetting() {
+        tabLayout.setOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+                viewPager.setCurrentItem(tab.getPosition());
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
+    }
+
+    /** fragment pager adapter */
+    static class ViewPagerAdapter extends FragmentPagerAdapter {
+        private final List<Fragment> mFragmentList = new ArrayList<>();
+        private final List<String> mFragmentTitleList = new ArrayList<>();
+
+        public ViewPagerAdapter(FragmentManager manager){
+            super(manager);
+        }
+
+        @Override
+        public Fragment getItem(int position) {
+            return mFragmentList.get(position);
+        }
+
+        @Override
+        public int getCount() {
+            return mFragmentList.size();
+
+        }
+
+        public void addFrag(Fragment fragment, String title){
+            mFragmentList.add(fragment);
+            mFragmentTitleList.add(title);
+        }
+
+        @Override
+        public CharSequence getPageTitle(int position){
+            return mFragmentTitleList.get(position);
+        }
+    }
+
+
 }
